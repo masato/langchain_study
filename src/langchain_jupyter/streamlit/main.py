5180e1f8e2b65aa3fcc8b80f3c2b3221990b3ebf
Masato Shimizu <ma6ato@gmail.com>
2024-01-06 00:59:24 +0900

    streamlit を追加

---
+++ src/langchain_jupyter/streamlit/main.py
@@ -0,0 +1,39 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import PromptTemplate
+from pydantic.v1 import BaseModel, Field
+
+load_dotenv()
+
+
+class Journey(BaseModel):
+    belongings: list[str] = Field(description="持ち物")
+    route: list[str] = Field(description="ルート")
+
+
+output_parser = PydanticOutputParser(pydantic_object=Journey)
+
+template = """旅行のプランを考えてください。
+
+{format_instructions}
+
+旅行先: {destination}
+"""
+
+prompt = PromptTemplate(
+    template=template,
+    input_variables=["destination"],
+    partial_variables={"format_instructions": output_parser.get_format_instructions()},
+)
+
+llm = ChatOpenAI(
+    model=os.environ["OPENAI_API_MODEL"],
+    temperature=float(os.environ["OPENAI_API_TEMPERATURE"]),
+)
+
+chain = prompt | llm | output_parser
+journey = chain.invoke({"destination": "沖縄"})
+print(journey)
