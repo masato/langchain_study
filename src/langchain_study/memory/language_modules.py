bc53c1a5e2a09dd88d51a47db4274f3826d455b9
Masato Shimizu <ma6ato@gmail.com>
2024-01-07 16:51:13 +0900

    Memory のサンプル

---
+++ src/langchain_study/memory/language_modules.py
@@ -0,0 +1,16 @@
+from langchain.schema import AIMessage, HumanMessage
+from langchain_openai import ChatOpenAI
+
+chat = ChatOpenAI()
+
+result = chat.invoke(
+    [
+        HumanMessage(content="茶碗蒸しの作り方を教"),
+        AIMessage(content="ChatModelからの返答である茶碗蒸しの作り方"),
+        HumanMessage(content="餃子の作り方を教えて"),
+        AIMessage(content="ChatModelからの返答である餃子の作り方"),
+        HumanMessage(content="チャーハンの作り方を教えて"),
+    ],
+)
+
+print(result.content)
